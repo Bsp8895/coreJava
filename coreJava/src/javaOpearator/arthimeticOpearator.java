aee492e32731107e27509e6b0c87bbf8d5c68301
bsp8895 <bhabaniparida1995@gmail.com>
2023-10-02 20:57:44 +0530

    arthimatic opearator first commit

---
+++ coreJava/src/javaOpearator/arthimeticOpearator.java
@@ -0,0 +1,18 @@
+package javaOpearator;
+
+public class arthimeticOpearator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		int c=30;
+		int d=40;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}
+
+}
