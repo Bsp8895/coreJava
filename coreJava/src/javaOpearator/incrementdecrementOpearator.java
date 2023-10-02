499030143e4fe12e28ac3179f91dba23c64384dc
bsp8895 <bhabaniparida1995@gmail.com>
2023-10-02 18:34:52 +0530

    IcrementdecrementOpearator1stcommit

---
+++ coreJava/src/javaOpearator/incrementdecrementOpearator.java
@@ -0,0 +1,34 @@
+package javaOpearator;
+
+public class incrementdecrementOpearator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		int c=30;
+		int d=40;
+		/*Pre Increment*/
+		int x1=++a;
+		System.out.println(x1);
+		System.out.println(a);
+		
+		/*Post Increment*/
+		int x2=b++;
+		System.out.println(x2);
+		System.out.println(b);
+		
+		/*Pre Decrement*/
+		int x3=--c;
+		System.out.println(x3);
+		System.out.println(c);
+		
+		/*Post Decrement*/
+		int x4=d--;
+		System.out.println(x4);
+		System.out.println(d);
+		
+		
+	}
+
+}
