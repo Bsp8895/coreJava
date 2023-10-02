e791e9f244a5567f92b270175f3cf6b6f49fd3dc
bsp8895 <bhabaniparida1995@gmail.com>
2023-10-02 23:17:58 +0530

    Realtional Opearator first commit

---
+++ coreJava/src/javaOpearator/relationalOperator.java
@@ -0,0 +1,20 @@
+package javaOpearator;
+
+public class relationalOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=20;
+		int b=50;
+		System.out.println(a>10);
+		System.out.println(a<10);
+		System.out.println(b>=50);
+		System.out.println(b>=a);
+		System.out.println(b<=a);
+		System.out.println('a'>a);
+		System.out.println('A'>a);
+		
+	
+	}
+
+}
