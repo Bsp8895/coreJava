eefba802b6a9d0343e53419484f3c8011da73bfe
bsp8895 <bhabaniparida1995@gmail.com>
2023-10-02 23:06:43 +0530

    concatination Opearator first commit

---
+++ coreJava/src/javaOpearator/concatenationOperator.java
@@ -0,0 +1,19 @@
+package javaOpearator;
+
+public class concatenationOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a="Durga";
+		int b=20;
+		int c=30;
+		int d=40;
+		System.out.println(a+b+c+d);
+		System.out.println(b+c+d+a);
+		System.out.println(c+d+a+b);
+		System.out.println(d+a+b+c);
+		
+
+	}
+
+}
